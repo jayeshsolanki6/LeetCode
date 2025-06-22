74c82fb40b86de6270935721dd35f3a049942bb3
jayeshsolanki6 <mejayeshsolanki@gmail.com>
2025-06-22 18:44:55 +0530

    Time: 0 ms (100%), Space: 50.1 MB (80.22%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -1,22 +1,47 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
         int len = nums.length;
-        int low = 0, high = len-1;
+        if(len == 1) return nums[0];
+        if(nums[0] != nums[1]) return nums[0];
+        if(nums[len-1] != nums[len-2]) return nums[len-1];
+        int low = 1, high = len-2;
         while(low<=high){
-            if(low == high){
-                return nums[low];
+            int mid = (low+high)/2;
+            if(nums[mid] != nums[mid-1] && nums[mid] != nums[mid+1]){
+                return nums[mid];
             }
-            if(nums[low] != nums[low+1]){
-                return nums[low];
+            if(nums[mid] != nums[mid+1]){
+                if(mid%2 == 0){
+                    high = mid-2;
+                } else{
+                    low = mid+1;
+                }
             } else{
-                low+=2;
-            }
-            if(nums[high] != nums[high-1]){
-                return nums[high];
-            } else{
-                high-=2;
+                if(mid%2 == 0){
+                    low = mid + 2;
+                } else{
+                    high = mid-1;
+                }
             }
         }
         return -1;
     }
 }
+//         int len = nums.length;
+//         int low = 0, high = len-1;
+//         while(low<=high){
+//             if(low == high){
+//                 return nums[low];
+//             }
+//             if(nums[low] != nums[low+1]){
+//                 return nums[low];
+//             } else{
+//                 low+=2;
+//             }
+//             if(nums[high] != nums[high-1]){
+//                 return nums[high];
+//             } else{
+//                 high-=2;
+//             }
+//         }
+//         return -1;
