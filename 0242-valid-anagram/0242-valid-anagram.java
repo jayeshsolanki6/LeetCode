ec29e6ac8bac1c22fd56567d8cd4449432785330
jayeshsolanki6 <mejayeshsolanki@gmail.com>
2025-06-27 08:39:33 +0530

    Time: 2 ms (97.93%), Space: 43.2 MB (70.87%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int len = s.length();
+        if(len != t.length()) return false;
+        int[] arr1 = new int[27];
+        int[] arr2 = new int[27];
+        // for(int i = 0; i<len; i++){
+        //     arr1[s.charAt(i) - 'a']++;
+        //     arr2[t.charAt(i) - 'a']++;
+        // }
+        for(char c: s.toCharArray()){
+            arr1[c - 'a']++;
+        }
+        for(char c: t.toCharArray()){
+            arr2[c - 'a']++;
+        }
+        for(int i = 0; i<27; i++){
+            if(arr1[i] != arr2[i]) return false;
+        }
+        return true;
+    }
+}
