4eeaca3b46c9a8b38ba049fcbbb37fe8db250e65
jayeshsolanki6 <mejayeshsolanki@gmail.com>
2025-06-27 08:44:29 +0530

    Time: 2 ms (97.93%), Space: 43.4 MB (63.21%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -3,7 +3,7 @@ class Solution {
         int len = s.length();
         if(len != t.length()) return false;
         int[] arr1 = new int[27];
-        int[] arr2 = new int[27];
+        // int[] arr2 = new int[27];
         // for(int i = 0; i<len; i++){
         //     arr1[s.charAt(i) - 'a']++;
         //     arr2[t.charAt(i) - 'a']++;
@@ -11,11 +11,15 @@ class Solution {
         for(char c: s.toCharArray()){
             arr1[c - 'a']++;
         }
+        // for(char c: t.toCharArray()){
+        //     arr2[c - 'a']++;
+        // }
+        // for(int i = 0; i<27; i++){
+        //     if(arr1[i] != arr2[i]) return false;
+        // }
         for(char c: t.toCharArray()){
-            arr2[c - 'a']++;
-        }
-        for(int i = 0; i<27; i++){
-            if(arr1[i] != arr2[i]) return false;
+            if(arr1[c - 'a'] == 0) return false;
+            arr1[c - 'a']--;
         }
         return true;
     }
