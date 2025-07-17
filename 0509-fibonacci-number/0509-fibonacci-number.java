bf8f2f75deaa6f42587011000840214b6bc24390
jayeshsolanki6 <mejayeshsolanki@gmail.com>
2025-07-17 15:42:09 +0530

    Time: 0 ms (100%), Space: 40.5 MB (49.56%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,19 +1,25 @@
 class Solution {
+    int[] memo;
     public int fib(int n) {
-        // return f(n);
-        if(n<=1)return n;
-        int first = 0, sec = 1;
-        int fib = 0;
-        while(n-1>0){
-            fib = first+sec;
-            first = sec;
-            sec = fib;
-            n--;
-        }
-        return fib;
+        memo = new int[n+1];
+        Arrays.fill(memo, -1);
+        return f(n);
+        // if(n<=1)return n;
+        // int first = 0, sec = 1;
+        // int fib = 0;
+        // while(n-1>0){
+        //     fib = first+sec;
+        //     first = sec;
+        //     sec = fib;
+        //     n--;
+        // }
+        // return fib;
     }
-    // public static int f(int n){
-    //     if(n<=1) return n;
-    //     return f(n-1) + f(n-2);
-    // }
+    int f(int n){
+        if(n<=1) return n;
+        if(memo[n] != -1) return memo[n];
+        int fact = f(n-1) + f(n-2);
+        memo[n] = fact;
+        return fact;
+    }
 }
