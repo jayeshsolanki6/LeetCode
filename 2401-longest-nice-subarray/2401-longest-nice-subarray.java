1dca97ff045be7a06b0f2b741066f805df02b8fa
jayeshsolanki6 <mejayeshsolanki@gmail.com>
2025-07-31 22:29:39 +0530

    Time: 4 ms (25.39%), Space: 55.5 MB (83.73%) - LeetHub

--- 2401-longest-nice-subarray/2401-longest-nice-subarray.java
+++ 2401-longest-nice-subarray/2401-longest-nice-subarray.java
@@ -1,29 +1,75 @@
 class Solution {
     public int longestNiceSubarray(int[] nums) {
-        int len = nums.length;
-        if(len == 1) return 1;
-        int xor = nums[0], sum = nums[0];
-        int count = 1, max = 1;
-        for(int i = 1; i<len; i++){
-            if((nums[i-1] & nums[i]) == 0){
-                sum += nums[i];
-                xor ^= nums[i];
-                if(sum == xor){
-                    count++;
-                } else{
-                    max = Math.max(max, count);
-                    count = 2;
-                    sum = nums[i] + nums[i-1];
-                    xor = nums[i] + nums[i-1];
+        // int len = nums.length;
+        // if(len == 1) return 1;
+        // int xor = nums[0], sum = nums[0];
+        // int count = 1, max = 1;
+        // for(int i = 1; i<len; i++){
+        //     if((nums[i-1] & nums[i]) == 0){
+        //         sum += nums[i];
+        //         xor ^= nums[i];
+        //         if(sum == xor){
+        //             count++;
+        //         } else{
+        //             max = Math.max(max, count);
+        //             count = 2;
+        //             sum = nums[i] + nums[i-1];
+        //             xor = nums[i] + nums[i-1];
+        //         }
+        //     } else{
+        //         max = Math.max(max, count);
+        //         count = 1;
+        //         sum = nums[i];
+        //         xor = nums[i];
+        //     }
+        //     max = Math.max(max, count);
+        // }
+        // return max;
+
+        // int len = nums.length;
+        // if(len == 1) return 1;
+        
+        // int xor = nums[0], sum = nums[0];
+        // int count = 1, max = 1;
+        
+        // for(int i = 1; i < len; i++){
+        //     // Try to extend current subarray
+        //     int newSum = sum + nums[i];
+        //     int newXor = xor ^ nums[i];
+            
+        //     if(newSum == newXor) {
+        //         // Valid extension - all pairs in subarray have & = 0
+        //         sum = newSum;
+        //         xor = newXor;
+        //         count++;
+        //     } else {
+        //         // Can't extend, start new subarray
+        //         max = Math.max(max, count);
+        //         count = 1;
+        //         sum = nums[i];
+        //         xor = nums[i];
+        //     }
+        // }
+        
+        // return Math.max(max, count);
+        int maxLen = 1;
+        
+        for (int i = 0; i < nums.length; i++) {
+            int sum = nums[i];
+            int xor = nums[i];
+            
+            for (int j = i + 1; j < nums.length; j++) {
+                sum += nums[j];
+                xor ^= nums[j];
+                
+                if (sum == xor) {
+                    maxLen = Math.max(maxLen, j - i + 1);
+                } else {
+                    break; // Can't extend further from position i
                 }
-            } else{
-                max = Math.max(max, count);
-                count = 1;
-                sum = nums[i];
-                xor = nums[i];
             }
-            max = Math.max(max, count);
         }
-        return max;
+        
+        return maxLen;
     }
 }
