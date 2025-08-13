ed3ff9673be2b28684cf5c0868abded7877db837
jayeshsolanki6 <mejayeshsolanki@gmail.com>
2025-08-13 08:54:02 +0530

    Time: 7 ms (100%), Space: 44.2 MB (51.08%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.java
+++ 0326-power-of-three/0326-power-of-three.java
@@ -4,6 +4,12 @@ class Solution {
         // double x = Math.log(n)/Math.log(3);
         // return Math.abs(x - Math.round(x)) < 1e-10;
 
-        return n > 0 && 1162261467 % n == 0;
+        // return n > 0 && 1162261467 % n == 0;
+
+        if(n < 1) return false;
+        while(n % 3 == 0){
+            n = n/3;
+        }
+        return n == 1;
     }
 }
