ca50c9b829fade467a12be8badf84fe5ead4aedc
jayeshsolanki6 <mejayeshsolanki@gmail.com>
2025-08-15 08:12:11 +0530

    Time: 4 ms (17.83%), Space: 46.9 MB (88.53%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,26 +1,37 @@
 class Solution {
     public int findCircleNum(int[][] isConnected) {
-        int len = isConnected.length;
-        int[] visited = new int[len];
+        int r = isConnected.length;
+        int c = isConnected[0].length;
+        List<List<Integer>> list = new ArrayList<>();
+        for(int i = 0; i<r; i++){
+            list.add(new ArrayList());
+            for(int j = 0; j<c; j++){
+                if(isConnected[i][j] == 1){
+                    list.get(i).add(j);
+                } 
+            }
+        }
+        int[] visited = new int[r];
         int count = 0;
-        for(int i = 0; i<len; i++){
+        for(int i = 0; i<r; i++){
             if(visited[i] == 0){
-                bfs(isConnected, visited, i, len);
                 count++;
+                traverse(list, i,visited);
             }
         }
         return count;
     }
-    void bfs(int[][] arr, int[] visited, int a, int len){
+    void traverse(List<List<Integer>> list, int a, int[] visited){
         Queue<Integer> queue = new LinkedList<>();
-        for(int i = 0; i<len; i++){
-            if(arr[a][i] == 1 && visited[i] == 0){
+        visited[a] = 1;
+        for(int i : list.get(a)){
+            if(visited[i] == 0){
                 queue.add(i);
                 visited[i] = 1;
             }
         }
         while(!queue.isEmpty()){
-            bfs(arr, visited, queue.remove(), len);
+            traverse(list, queue.remove(), visited);
         }
     }
 }
