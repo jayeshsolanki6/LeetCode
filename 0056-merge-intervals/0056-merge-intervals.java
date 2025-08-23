7df344b297e6f4f5337552d42207a3e3435d0f18
jayeshsolanki6 <mejayeshsolanki@gmail.com>
2025-08-23 11:08:36 +0530

    Time: 181 ms (5.13%), Space: 46.8 MB (25.85%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -4,19 +4,23 @@ class Solution {
         int count = 0;
         for(int i = 0; i<row; i++){
             if(grid[i][0] == -1) continue;
+            boolean change = false;
             for(int j = i+1; j<row; j++){
                 if(grid[j][0] == -1) continue;
                 if(grid[i][0] <= grid[j][0] && grid[i][1] >= grid[j][0]){
                     grid[i][1] = Math.max(grid[i][1], grid[j][1]);
                     grid[j][0] = -1;
                     count++;
+                    change = true;
                 } else if(grid[j][0] <= grid[i][0] && grid[j][1] >= grid[i][0]){
                     grid[i][0] = grid[j][0];
                     grid[i][1] = Math.max(grid[i][1], grid[j][1]);
                     grid[j][0] = -1;
                     count++;
+                    change = true;
                 }
             }
+            if(change) i--;
         }
         int[][] ans = new int[row-count][2];
         int j = 0;
