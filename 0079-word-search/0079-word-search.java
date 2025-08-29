a2c108897cfd1f8e0a9280c8e902c22502750d46
jayeshsolanki6 <mejayeshsolanki@gmail.com>
2025-08-29 22:56:59 +0530

    Time: 127 ms (90.65%), Space: 41.9 MB (36.74%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -14,7 +14,7 @@ class Solution {
         for(int i = 0; i<m; i++){
             for(int j = 0; j<n; j++){
                 if(board[i][j] == c){
-                    if(helper(i, j, 0, -1, -1)){
+                    if(helper(i, j, 0)){
                         return true;
                     }
                 }
@@ -22,21 +22,30 @@ class Solution {
         }
         return false;
     }
-    boolean helper(int i, int j, int ind, int pi, int pj){
+    boolean helper(int i, int j, int ind){
         if(ind == len) return true;
-        if(board[i][j] != word.charAt(ind)) return false;
-        if(ind == len-1) return true;
-        for(int k = 0; k<4; k++){
-            int[] d = dir[k];
-            int ii = i + d[0];
-            int jj = j + d[1];
-            if(ii == pi && jj == pj) continue;
-            if(ii >= 0 && ii < m && jj >= 0 && jj < n){
-                if(helper(ii, jj, ind+1, i, j)){
-                    return true;
-                }
-            }
+
+        if(i < 0 || j < 0 || i >= m || j >= n || board[i][j] != word.charAt(ind) || board[i][j] == '!'){
+            return false;
         }
-        return false;
+
+        char c = board[i][j];
+        board[i][j] = '!';
+
+        // boolean res = false;
+        // for(int k = 0; k<4; k++){
+        //     int[] d = dir[i];
+        //     int ni = i+d[0];
+        //     int nj = j+d[1];
+        //     res = res || helper(ni, nj, ind+1);
+        // }
+
+        boolean top = helper(i - 1, j, ind + 1);
+        boolean right = helper(i, j + 1, ind + 1);
+        boolean bottom = helper(i + 1, j, ind + 1);
+        boolean left = helper(i, j - 1, ind + 1);
+        board[i][j] = c;
+        // return res;
+        return top || right || bottom || left;
     }
 }
