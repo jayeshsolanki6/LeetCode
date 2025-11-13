a969d7944e4a4f0b9ba48a8eac506dbc16aed4bd
jayeshsolanki6 <mejayeshsolanki@gmail.com>
2025-11-13 12:14:42 +0530

    Time: 6 ms (100%), Space: 47.2 MB (27.41%) - LeetHub

--- 3228-maximum-number-of-operations-to-move-ones-to-the-end/3228-maximum-number-of-operations-to-move-ones-to-the-end.java
+++ 3228-maximum-number-of-operations-to-move-ones-to-the-end/3228-maximum-number-of-operations-to-move-ones-to-the-end.java
@@ -2,20 +2,11 @@ class Solution {
     public int maxOperations(String s) {
         int len = s.length();
         char[] arr = s.toCharArray();
-        int[] pre = new int[len];
         int one = 0;
+        int op = 0;
         for(int i = 0; i<len; i++){
             if(arr[i] == '1') one++;
-            else pre[i] = one;
-        }
-        int op = 0;
-        for(int i = len-1; i>0; i--){
-            if(arr[i] == '0'){
-                if(arr[i-1] == '0'){
-                    continue;
-                }
-                op += pre[i];
-            }
+            else if(i == len-1 || arr[i+1] == '1') op += one;
         }
         return op;
     }
