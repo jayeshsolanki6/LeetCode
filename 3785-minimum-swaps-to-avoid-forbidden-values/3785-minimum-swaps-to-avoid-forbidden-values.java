f9ef903c7a073e781fe3234c6f91d1ec789f2433
jayeshsolanki6 <mejayeshsolanki@gmail.com>
2025-12-21 15:03:36 +0530

    Time: 102 ms (50%), Space: 185.7 MB (50%) - LeetHub

--- 3785-minimum-swaps-to-avoid-forbidden-values/3785-minimum-swaps-to-avoid-forbidden-values.java
+++ 3785-minimum-swaps-to-avoid-forbidden-values/3785-minimum-swaps-to-avoid-forbidden-values.java
@@ -4,7 +4,16 @@ class Solution {
         Map<Integer, Integer> n = new HashMap<>();
         Map<Integer, Integer> f = new HashMap<>();
         int res = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        int bad = 0, maxB = 0;
+
         for(int i = 0; i<len; i++){
+            if(nums[i] == forb[i]){
+                bad++;
+                int c = map.getOrDefault(nums[i], 0) + 1;
+                map.put(nums[i], c);
+                maxB = Math.max(maxB, c);
+            }
             n.put(nums[i], n.getOrDefault(nums[i], 0)+1);
             f.put(forb[i], f.getOrDefault(forb[i], 0)+1);
         }
@@ -12,16 +21,6 @@ class Solution {
         for(int key : n.keySet()){
             if(f.containsKey(key) && f.get(key) > len-n.get(key)){
                 return -1;
-            }
-        }
-        Map<Integer, Integer> map = new HashMap<>();
-        int bad = 0, maxB = 0;
-        for(int i = 0; i<len; i++){
-            if(nums[i] == forb[i]){
-                bad++;
-                int c = map.getOrDefault(nums[i], 0) + 1;
-                map.put(nums[i], c);
-                maxB = Math.max(maxB, c);
             }
         }
         return Math.max(maxB, (bad+1)/2);
