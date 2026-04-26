7f0d0e68d03ab68de16366225cafaa25405eae80
jayeshsolanki6 <mejayeshsolanki@gmail.com>
2026-04-26 13:46:42 +0530

    Time: 1 ms (37.75%), Space: 46.6 MB (79.17%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,38 +1,17 @@
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        int m = image.length;
-        int n = image[0].length;
-        int can = image[sr][sc];
-        // image[sr][sc] = color;
-        // Queue<int[]> queue = new LinkedList<>();
-        // queue.add(new int[]{sr, sc});
-        int[][] direction = new int[][]{{0, -1}, {0, 1}, {-1, 0}, {1, 0}};
-        dfs(image, direction, sr, sc, color, can, m, n);
-        // while(!queue.isEmpty()){
-        //     int size = queue.size();
-        //     while(size-- > 0){
-        //         int[] curr = queue.remove();
-        //         int i = curr[0];
-        //         int j = curr[1];
-        //         for(int[] dir : direction){
-        //             int ii = i + dir[0];
-        //             int jj = j + dir[1];
-        //             if(ii >= 0 && jj >= 0 && ii < m && jj < n && image[ii][jj] == can && image[ii][jj] != color){
-        //                 image[ii][jj] = color;
-        //                 queue.add(new int[]{ii, jj});
-        //             }
-        //         }
-        //     }
-        // }
+        if(image[sr][sc] == color) return image;
+        int[][] dir = new int[][]{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+        dfs(sr, sc, color, image[sr][sc], image, dir);
         return image;
     }
-    void dfs(int[][] image, int[][] direction, int row, int col, int color, int can, int m, int n){
-        image[row][col] = color;
-        for(int[] dir : direction){
-            int ii = row + dir[0];
-            int jj = col + dir[1];
-            if(ii >= 0 && jj >= 0 && ii < m && jj < n && image[ii][jj] == can && image[ii][jj] != color){
-                dfs(image, direction, ii, jj, color, can, m, n);
+    void dfs(int x, int y, int c, int pc, int[][] image, int[][] dir){
+        image[x][y] = c;
+
+        for(int[] d : dir){
+            int i = x + d[0], j = y + d[1];
+            if(i >= 0 && j >= 0 && i < image.length && j < image[0].length && image[i][j] == pc){
+                dfs(i, j, c, pc, image, dir);
             }
         }
     }
