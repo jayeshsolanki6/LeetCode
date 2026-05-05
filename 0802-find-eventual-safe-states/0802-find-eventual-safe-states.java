039151470c28b32cd61ca64232a5a60babf902e8
jayeshsolanki6 <mejayeshsolanki@gmail.com>
2026-05-05 10:19:59 +0530

    Time: 4 ms (99.81%), Space: 63.3 MB (35.87%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.java
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.java
@@ -2,30 +2,26 @@ class Solution {
     public List<Integer> eventualSafeNodes(int[][] graph) {
         int nodes = graph.length;
         int[] vis = new int[nodes];
-
-        Set<Integer> set = new HashSet<>();
+ 
+        List<Integer> res = new ArrayList<>();
 
         for(int i = 0; i<nodes; i++){
             if(vis[i] == 0){
-                if(dfs(i, graph, vis, set)){
-                    set.add(i);
+                if(!dfs(i, graph, vis)){
+                    res.add(i);
                 }
+            } else if(vis[i] == 1){
+                res.add(i);
             }
         }
-        List<Integer> res = new ArrayList<>();
-        for(int i = 0; i<nodes; i++){
-            if(!set.contains(i)) res.add(i);
-        }
+
         return res;
     }
-    boolean dfs(int n, int[][] graph, int[] vis, Set<Integer> set){
+    boolean dfs(int n, int[][] graph, int[] vis){
         vis[n] = 2;
         for(int adj : graph[n]){
-            if(vis[adj] == 0){
-                if(dfs(adj, graph, vis, set)){
-                    set.add(adj);
-                    return true;
-                }
+            if(vis[adj] == 0 && dfs(adj, graph, vis)){
+                return true;
             }else if(vis[adj] == 2){
                 return true;
             }
