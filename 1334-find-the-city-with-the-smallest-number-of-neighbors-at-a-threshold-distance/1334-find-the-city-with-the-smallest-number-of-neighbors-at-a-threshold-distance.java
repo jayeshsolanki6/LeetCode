787db181e63fb353e9512b6ef2febcb272f02f94
jayeshsolanki6 <mejayeshsolanki@gmail.com>
2026-05-14 09:05:09 +0530

    Time: 52 ms (11.01%), Space: 46.9 MB (17.41%) - LeetHub

--- 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.java
+++ 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.java
@@ -1,42 +1,54 @@
 class Solution {
     public int findTheCity(int n, int[][] edges, int distanceThreshold) {
-        int[][] dp = new int[n][n];
+        List<List<int[]>> list = new ArrayList<>();
+        for(int i = 0; i<n; i++) list.add(new ArrayList<>());
         for(int[] edge : edges){
             int u = edge[0], v = edge[1], wt = edge[2];
-            dp[u][v] = wt;
-            dp[v][u] = wt;
-        }
-        int max = Integer.MAX_VALUE;
-        for(int i = 0; i<n; i++){
-            for(int j = 0; j<n; j++){
-                if(i == j) continue;
-                if(dp[i][j] == 0) dp[i][j] = max;
-            }
+            list.get(u).add(new int[]{v, wt});
+            list.get(v).add(new int[]{u, wt});
         }
 
-        for(int k = 0; k<n; k++){
-            for(int i = 0; i<n; i++){
-                for(int j = 0; j<n; j++){
-                    if(dp[i][k] != max && dp[k][j] != max && dp[i][j] > dp[i][k] + dp[k][j]){
-                        dp[i][j] = dp[i][k] + dp[k][j];
-                    }
-                }
-            }
-        }
-
-        int minCities = max;
+        int minCities = Integer.MAX_VALUE;
         int city = -1;
 
         for(int i = 0; i<n; i++){
+            int[] dist = new int[n];
+            Arrays.fill(dist, Integer.MAX_VALUE);
+            dist[i] = 0;
+
+            dijkstra(i, list, dist);
             int count = 0;
-            for(int j = 0; j<n; j++){
-                if(dp[i][j] <= distanceThreshold) count++;
+            for(int d : dist){
+                if(d <= distanceThreshold){
+                    count++;
+                }
             }
             if(count <= minCities){
                 minCities = count;
                 city = i;
             }
         }
+
         return city;
+    }
+
+    void dijkstra(int src, List<List<int[]>> list, int[] dist){
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> Integer.compare(a[1], b[1]));
+        pq.add(new int[]{src, 0});
+
+        while(!pq.isEmpty()){
+            int[] pair = pq.remove();
+            int node = pair[0];
+            int dis = pair[1];
+
+            for(int[] adj : list.get(node)){
+                int newDist = dis + adj[1];
+                if(newDist < dist[adj[0]]){
+                    dist[adj[0]] = newDist;
+                    pq.add(new int[]{adj[0], newDist});
+                }
+            }
+        }
+        return;
     }
 }
