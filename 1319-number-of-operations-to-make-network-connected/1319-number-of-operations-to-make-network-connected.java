46c39ed52b2bd835649421d94aee6ee05fa594c4
jayeshsolanki6 <mejayeshsolanki@gmail.com>
2026-05-15 10:00:00 +0530

    Time: 18 ms (18.49%), Space: 74.2 MB (9.35%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.java
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.java
@@ -1,65 +1,33 @@
 class Solution {
     public int makeConnected(int n, int[][] connections) {
-        DisjointSet ds = new DisjointSet(n);
+        if(connections.length < n-1) return -1;
+
+        List<List<Integer>> list = new ArrayList<>();
+        for(int i = 0; i<n; i++) list.add(new ArrayList<>());
+
         for(int[] edge : connections){
-            ds.unionBySize(edge);
+            list.get(edge[0]).add(edge[1]);
+            list.get(edge[1]).add(edge[0]);
         }
-        return ds.getResult();
-    }
-}
-class DisjointSet{
-    int n;
-    int[] size;
-    int[] parent;
-    int extraCable = 0;
 
-    DisjointSet(int n){
-        this.n = n;
-        size = new int[n];
-        Arrays.fill(size, 1);
+        boolean[] vis = new boolean[n];
+
+        int components = 0;
+        for(int i = 0; i<n; i++){
+            if(!vis[i]){
+                components++;
+                dfs(i, list, vis);
+            }
+        }
         
-        parent = new int[n];
-        for(int i = 0; i<n; i++){
-            parent[i] = i;
-        }
+        return components-1;
     }
+    void dfs(int node, List<List<Integer>> list, boolean[] vis){
+        if(vis[node]) return;
+        vis[node] = true;
 
-    int findUPar(int n){
-        if(parent[n] == n) return n;
-        return parent[n] = findUPar(parent[n]);
-    }
-
-    void unionBySize(int[] edge){
-        int u = edge[0], v = edge[1];
-        int utp_u = findUPar(u);
-        int utp_v = findUPar(v);
-
-        if(utp_u == utp_v){
-            extraCable++;
-            return;
+        for(int adj : list.get(node)){
+            dfs(adj, list, vis);
         }
-        int size_u = size[utp_u];
-        int size_v = size[utp_v];
-
-        if(size_u > size_v){
-            parent[utp_v] = utp_u;
-            size[utp_u] += size_v;
-        } else{
-            parent[utp_u] = utp_v;
-            size[utp_v] += size_u;
-        }
-    }
-
-    int getResult(){
-        Set<Integer> set = new HashSet<>();
-        for(int i = 0; i<n; i++){
-            set.add(findUPar(i));
-        }
-
-        int cableReq = set.size()-1;
-        if(cableReq <= extraCable){
-            return cableReq;
-        }
-        return -1;
     }
 }
