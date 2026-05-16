66927e570cc959642b33e607012bebf8203d45ab
jayeshsolanki6 <mejayeshsolanki@gmail.com>
2026-05-16 07:30:24 +0530

    Time: 5 ms (97.39%), Space: 46.8 MB (72.43%) - LeetHub

--- 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.java
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.java
@@ -1,44 +1,20 @@
 class Solution {
+    int[] parent;
     public int removeStones(int[][] stones) {
-        int len = stones.length;
-        int x = 0, y = 0;
-        for(int[] pair : stones){
-            x = Math.max(x, pair[0]);
-            y = Math.max(y, pair[1]);
-        }
-
-        DisjointSet ds = new DisjointSet(x + y + 2);
-
-        Set<Integer> set = new HashSet<>();
+    
+        parent = new int[20002];
+        for(int i = 0; i<20002; i++) parent[i] = i;
 
         for(int[] pair : stones){
-            int i = pair[0];
-            int j = pair[1] + x + 1;
-            ds.unionBySize(i, j);
-            set.add(i);
-            set.add(j);
+            int x = pair[0], y = pair[1] + 10001;
+            union(x, y);
+        }
+        Set<Integer> com = new HashSet<>();
+        for(int[] stone : stones){
+            com.add(findUPar(stone[0]));
         }
 
-        int com = 0;
-
-        for(int node : set){
-            if(ds.findUPar(node) == node) com++;
-        }
-        return len - com;
-    }
-
-}
-
-class DisjointSet{
-    int[] size, parent;
-
-    DisjointSet(int n){
-        size = new int[n];
-        Arrays.fill(size, 1);
-        parent = new int[n];
-        for(int i = 0; i<n; i++){
-            parent[i] = i;
-        }
+        return stones.length - com.size();
     }
 
     int findUPar(int n){
@@ -46,21 +22,12 @@ class DisjointSet{
         return parent[n] = findUPar(parent[n]);
     }
 
-    void unionBySize(int u, int v){
-        int up = findUPar(u);
-        int vp = findUPar(v);
-
-        if(up == vp) return;
-
-        int size_u = size[up];
-        int size_v = size[vp];
-
-        if(size_u > size_v){
-            parent[vp] = up;
-            size[up] += size_v;
-        } else{
-            parent[up] = vp;
-            size[vp] += size_u;
+    void union(int x, int y){
+        int upx = findUPar(x);
+        int upy = findUPar(y);
+        if(upx != upy){
+            parent[upx] = upy;
         }
     }
+
 }
