f199d48ad62188fa0fb2e8ed450c5f4749dd5336
jayeshsolanki6 <mejayeshsolanki@gmail.com>
2026-05-16 07:15:43 +0530

    Time: 13 ms (72.03%), Space: 46.9 MB (47.3%) - LeetHub

---
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.java
@@ -0,0 +1,66 @@
+class Solution {
+    public int removeStones(int[][] stones) {
+        int len = stones.length;
+        int x = 0, y = 0;
+        for(int[] pair : stones){
+            x = Math.max(x, pair[0]);
+            y = Math.max(y, pair[1]);
+        }
+
+        DisjointSet ds = new DisjointSet(x + y + 2);
+
+        Set<Integer> set = new HashSet<>();
+
+        for(int[] pair : stones){
+            int i = pair[0];
+            int j = pair[1] + x + 1;
+            ds.unionBySize(i, j);
+            set.add(i);
+            set.add(j);
+        }
+
+        Set<Integer> com = new HashSet<>();
+
+        for(int node : set){
+            com.add(ds.findUPar(node));
+        }
+        return len - com.size();
+    }
+
+}
+
+class DisjointSet{
+    int[] size, parent;
+
+    DisjointSet(int n){
+        size = new int[n];
+        Arrays.fill(size, 1);
+        parent = new int[n];
+        for(int i = 0; i<n; i++){
+            parent[i] = i;
+        }
+    }
+
+    int findUPar(int n){
+        if(parent[n] == n) return n;
+        return parent[n] = findUPar(parent[n]);
+    }
+
+    void unionBySize(int u, int v){
+        int up = findUPar(u);
+        int vp = findUPar(v);
+
+        if(up == vp) return;
+
+        int size_u = size[up];
+        int size_v = size[vp];
+
+        if(size_u > size_v){
+            parent[vp] = up;
+            size[up] += size_v;
+        } else{
+            parent[up] = vp;
+            size[vp] += size_u;
+        }
+    }
+}
