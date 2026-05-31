9a7e43c27d049b510b34ff98b7e7fd70cb76497e
jayeshsolanki6 <mejayeshsolanki@gmail.com>
2026-05-31 06:47:00 +0530

    Time: 3 ms (88.6%), Space: 47.4 MB (82.86%) - LeetHub

--- 0135-candy/0135-candy.java
+++ 0135-candy/0135-candy.java
@@ -1,30 +1,19 @@
 class Solution {
     public int candy(int[] ratings) {
         int len = ratings.length;
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> a[1] - b[1]);
-
-        for(int i = 0; i<len; i++){
-            pq.add(new int[]{i, ratings[i]});
-        }
 
         int[] candies = new int[len];
-        Arrays.fill(candies, -1);
+        Arrays.fill(candies, 1);
 
-        while(!pq.isEmpty()){
-            int min = pq.remove()[0];
-            if(candies[min] != -1) continue;
-
-            candies[min] = 1;
-            int l = min-1;
-            int r = min+1;
-
-            while(l >= 0 && ratings[l] > ratings[l+1] && (candies[l] == -1 || candies[l] <= candies[l+1])){
-                candies[l] = candies[l+1]+1;
-                l--;
+        for(int i = 1; i<len; i++){
+            if(ratings[i] > ratings[i-1]){
+                candies[i] = candies[i-1] + 1;
             }
-            while(r < len && ratings[r] > ratings[r-1] && (candies[r] == -1 || candies[r] <= candies[r-1])){
-                candies[r] = candies[r-1]+1;
-                r++;
+        }
+
+        for(int i = len-2; i>=0; i--){
+            if(ratings[i] > ratings[i+1]){
+                candies[i] = Math.max(candies[i], candies[i+1] + 1);
             }
         }
         
