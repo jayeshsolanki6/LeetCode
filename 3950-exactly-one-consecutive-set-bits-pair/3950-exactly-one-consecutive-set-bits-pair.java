231138384356cab14a69cabd906494f595259883
jayeshsolanki6 <mejayeshsolanki@gmail.com>
2026-06-10 08:13:09 +0530

    Time: 0 ms (100%), Space: 41.9 MB (99.81%) - LeetHub

--- 3950-exactly-one-consecutive-set-bits-pair/3950-exactly-one-consecutive-set-bits-pair.java
+++ 3950-exactly-one-consecutive-set-bits-pair/3950-exactly-one-consecutive-set-bits-pair.java
@@ -1,13 +1,6 @@
 class Solution {
     public boolean consecutiveSetBits(int n) {
-        char[] s = Integer.toBinaryString(n).toCharArray();
-
-        int len = s.length;
-        int count = 0;
-        for(int i = 1; i<len; i++){
-            if(s[i-1] == '1' && s[i] == '1') count++;
-        }
-
-        return count == 1;
+        int pair = n & (n >> 1);
+        return pair > 0 && (pair & (pair-1)) == 0;
     }
 }
