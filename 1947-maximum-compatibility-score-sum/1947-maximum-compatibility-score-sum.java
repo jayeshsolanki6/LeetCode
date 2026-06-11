407e53a67e9aa5f0e372b9d5adf5392c829f648f
jayeshsolanki6 <mejayeshsolanki@gmail.com>
2026-06-11 07:29:13 +0530

    Time: 1 ms (100%), Space: 43.4 MB (33.33%) - LeetHub

--- 1947-maximum-compatibility-score-sum/1947-maximum-compatibility-score-sum.java
+++ 1947-maximum-compatibility-score-sum/1947-maximum-compatibility-score-sum.java
@@ -2,13 +2,18 @@ class Solution {
     public int maxCompatibilitySum(int[][] students, int[][] mentors) {
         int m = students.length;
         int n = students[0].length;
-        boolean[] vis = new boolean[m];
-
-        return find(0, 0, students, mentors, m, n);
+        int[][] dp = new int[m][1 << m];
+        for(int[] d : dp){
+            Arrays.fill(d, -1);
+        }
+        return find(0, 0, students, mentors, dp);
     }
-    int find(int i, int mask, int[][] students, int[][] mentors, int m, int n){
+    int find(int i, int mask, int[][] students, int[][] mentors, int[][] dp) {
+        int m = students.length;
+        int n = students[0].length;
         if(i == m) return 0;
-        
+        if(dp[i][mask] != -1) return dp[i][mask];
+
         int max = Integer.MIN_VALUE;
 
         for(int j = 0; j<m; j++){
@@ -17,10 +22,10 @@ class Solution {
                 for(int k = 0; k<n; k++){
                     score += ((students[i][k] ^ mentors[j][k]) == 0) ? 1 : 0;
                 }
-                max = Math.max(max, score + find(i+1, (1 << j)|mask, students, mentors, m, n));
+                max = Math.max(max, score + find(i+1, (1 << j)|mask, students, mentors, dp));
             }
         }
 
-        return max;
+        return dp[i][mask] = max;
     }
 }
