9b6da4b9af85b809f07b0c32ac91b00070fedc67
jayeshsolanki6 <mejayeshsolanki@gmail.com>
2026-06-15 09:40:45 +0530

    Time: 432 ms (7.66%), Space: 47.3 MB (61.68%) - LeetHub

---
+++ 0732-my-calendar-iii/0732-my-calendar-iii.java
@@ -0,0 +1,26 @@
+class MyCalendarThree {
+    TreeMap<Integer, Integer> map;
+    public MyCalendarThree() {
+        map = new TreeMap<>();
+    }
+    
+    public int book(int startTime, int endTime) {
+        map.put(startTime, map.getOrDefault(startTime, 0)+1);
+        map.put(endTime, map.getOrDefault(endTime, 0)-1);
+
+        int totalBooking = 0;
+        int maxBooking = 0;
+        for(int key : map.keySet()){
+            totalBooking += map.get(key);
+            maxBooking = Math.max(maxBooking, totalBooking);
+        }
+
+        return maxBooking;
+    }
+}
+
+/**
+ * Your MyCalendarThree object will be instantiated and called as such:
+ * MyCalendarThree obj = new MyCalendarThree();
+ * int param_1 = obj.book(startTime,endTime);
+ */
