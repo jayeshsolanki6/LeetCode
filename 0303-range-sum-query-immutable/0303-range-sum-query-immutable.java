20e43d3d2df20dba4bffe2b61f29a422e1834150
jayeshsolanki6 <mejayeshsolanki@gmail.com>
2026-06-18 17:21:09 +0530

    Time: 7 ms (100%), Space: 47.5 MB (90.45%) - LeetHub

--- 0303-range-sum-query-immutable/0303-range-sum-query-immutable.java
+++ 0303-range-sum-query-immutable/0303-range-sum-query-immutable.java
@@ -1,16 +1,16 @@
 class NumArray {
     int[] presum;
-    int n;
+
     public NumArray(int[] nums) {
         int n = nums.length;
-        presum = new int[n+1];
-        for(int i = 0; i<n; i++){
-            presum[i+1] = presum[i] + nums[i];
+        presum = new int[n + 1];
+        for (int i = 0; i < n; i++) {
+            presum[i + 1] = presum[i] + nums[i];
         }
     }
-    
+
     public int sumRange(int left, int right) {
-        return presum[right+1] - presum[left];
+        return presum[right + 1] - presum[left];
     }
 }
 
