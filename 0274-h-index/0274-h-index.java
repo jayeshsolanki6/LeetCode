b553061269802f46a61b37b7ae7713dce5128c20
jayeshsolanki6 <mejayeshsolanki@gmail.com>
2026-06-24 19:17:21 +0530

    Time: 0 ms (100%), Space: 43.1 MB (91.48%) - LeetHub

--- 0274-h-index/0274-h-index.java
+++ 0274-h-index/0274-h-index.java
@@ -1,12 +1,18 @@
 class Solution {
     public int hIndex(int[] citations) {
-        Arrays.sort(citations);
         int len = citations.length;
+        int[] count = new int[len+1];
 
         for(int i = 0; i<len; i++){
-            if(len-i <= citations[i]) return len-i;
+            if(citations[i] >= len) count[len]++;
+            else count[citations[i]]++;
+        }
+        int sum = 0;
+        for(int i = len; i>=0; i--){
+            sum += count[i];
+            if(sum >= i) return i;
         }
 
-        return 0;
+        return -1;
     }
 }
