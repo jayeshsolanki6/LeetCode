abff4b5d928dab289ea17c67a659e28a7a7a321b
jayeshsolanki6 <mejayeshsolanki@gmail.com>
2026-06-25 07:08:06 +0530

    Time: 1 ms (99.71%), Space: 47.4 MB (21.14%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,19 +1,19 @@
 class Solution {
     public int jump(int[] nums) {
-        int[] dp = new int[nums.length];
-        Arrays.fill(dp, -1);
-        return find(0, nums, nums.length, dp);
-    }
-    int find(int i, int[] nums, int len, int[] dp){
-        if(i == len-1) return 0;
-        if(dp[i] != -1) return dp[i];
-        int min = Integer.MAX_VALUE/2;
+        int len = nums.length;
+        int jump = 0;
+        int l = 0, r = 0;
 
-        int reach = Math.min(len-1, i + nums[i]);
-        for(int j = i+1; j<=reach; j++){
-            min = Math.min(min, 1+find(j, nums, len, dp));
+        while(r < len-1){
+            int maxReach = l;
+            for(int i = l; i<=r; i++){
+                maxReach = Math.max(maxReach, i+nums[i]);
+            }
+            l = r+1;
+            r = maxReach;
+            jump++;
         }
 
-        return dp[i] = min;
+        return jump;
     }
 }
