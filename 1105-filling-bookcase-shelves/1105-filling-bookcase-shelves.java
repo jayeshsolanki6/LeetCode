043b6177981dfca84f993cb826580c19ca569810
jayeshsolanki6 <mejayeshsolanki@gmail.com>
2026-07-15 07:26:33 +0530

    Time: 0 ms (100%), Space: 43.9 MB (63.09%) - LeetHub

--- 1105-filling-bookcase-shelves/1105-filling-bookcase-shelves.java
+++ 1105-filling-bookcase-shelves/1105-filling-bookcase-shelves.java
@@ -3,25 +3,24 @@ class Solution {
         int len = books.length;
         int[] dp = new int[len+1]; 
         Arrays.fill(dp, -1);
-        return find(0, shelfWidth, books, len, dp);
+        return find(len-1, shelfWidth, books, dp);
     }
 
-    int find(int i, int width, int[][] books, int len, int[] dp){
-        if(i == len) return 0;
+    int find(int i, int width, int[][] books, int[] dp){
+        if(i < 0) return 0;
 
         if(dp[i] != -1) return dp[i];
 
         int minH = Integer.MAX_VALUE;
         int h = 0;
         int w = 0;
-        int temp = i;
-        while(i < len && (w + books[i][0] <= width)){
-            w += books[i][0];
-            h = Math.max(h, books[i][1]);
-            i++;
-            minH = Math.min(minH, h + find(i, width, books, len, dp));
+        for(int j = i; j >= 0; j--){
+            if(w + books[j][0] > width) break;
+            w += books[j][0];
+            h = Math.max(h, books[j][1]);
+            minH = Math.min(minH, h + find(j-1, width, books, dp));
         }
 
-        return dp[temp] = minH;
+        return dp[i] = minH;
     }
 }
