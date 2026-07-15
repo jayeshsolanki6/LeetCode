be61c8f6c33c63f492f9b04f9bf098fd278670c9
jayeshsolanki6 <mejayeshsolanki@gmail.com>
2026-07-15 07:23:35 +0530

    Time: 0 ms (100%), Space: 43.6 MB (93.56%) - LeetHub

--- 1105-filling-bookcase-shelves/1105-filling-bookcase-shelves.java
+++ 1105-filling-bookcase-shelves/1105-filling-bookcase-shelves.java
@@ -1,15 +1,15 @@
 class Solution {
     public int minHeightShelves(int[][] books, int shelfWidth) {
         int len = books.length;
-        HashMap<String, Integer> dp = new HashMap<>(); 
-        return find(0, 0, shelfWidth, books, len, dp);
+        int[] dp = new int[len+1]; 
+        Arrays.fill(dp, -1);
+        return find(0, shelfWidth, books, len, dp);
     }
 
-    int find(int i, int shelf, int width, int[][] books, int len, HashMap<String, Integer> dp){
+    int find(int i, int width, int[][] books, int len, int[] dp){
         if(i == len) return 0;
 
-        String s = i + "" + shelf;
-        if(dp.containsKey(s)) return dp.get(s);
+        if(dp[i] != -1) return dp[i];
 
         int minH = Integer.MAX_VALUE;
         int h = 0;
@@ -19,9 +19,9 @@ class Solution {
             w += books[i][0];
             h = Math.max(h, books[i][1]);
             i++;
-            minH = Math.min(minH, h + find(i, shelf+1, width, books, len, dp));
+            minH = Math.min(minH, h + find(i, width, books, len, dp));
         }
-        dp.put(s, minH);
-        return minH;
+
+        return dp[temp] = minH;
     }
 }
