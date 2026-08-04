b4eb05902b1516e59f507d3535f4fa1d0e42eceb
jayeshsolanki6 <mejayeshsolanki@gmail.com>
2026-08-04 11:54:57 +0530

    Time: 5 ms (82.12%), Space: 46.7 MB (67.16%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.java
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -1,40 +1,35 @@
 class Solution {
     public int[] findOrder(int numCourses, int[][] prerequisites) {
         List<List<Integer>> list = new ArrayList<>();
+        for(int i = 0; i<numCourses; i++) list.add(new ArrayList<>());
 
-        for(int i = 0; i<numCourses; i++){
-            list.add(new ArrayList<>());
-        }
-
-        for(int[] edge : prerequisites){
-            list.get(edge[0]).add(edge[1]);
-        }
-
-        int[] vis = new int[numCourses];
-        List<Integer> l = new ArrayList<>();
-        for(int i = 0; i<numCourses; i++){
-            if(vis[i] == 0 && dfs(i, list, vis, l)){
-                return new int[]{};
-            }
+        int[] inDeg = new int[numCourses];
+        for(int[] pre : prerequisites){
+            list.get(pre[1]).add(pre[0]);
+            inDeg[pre[0]]++;
         }
         int[] res = new int[numCourses];
-        for(int i = 0; i<numCourses; i++){
-            res[i] = l.get(i);
+        int i = 0;
+        Queue<Integer> q = new ArrayDeque<>();
+        for(int j = 0; j<numCourses; j++){
+            if(inDeg[j] == 0) q.add(j);
         }
-        return res;
-    }
-    boolean dfs(int i, List<List<Integer>> list, int[] vis, List<Integer> l){
-        vis[i] = 2;
-        List<Integer> adj = list.get(i);
-        for(int x : adj){
-            if(vis[x] == 0 && dfs(x, list, vis, l)){
-                return true;
-            } else if(vis[x] == 2){
-                return true;
+
+        while(!q.isEmpty()){
+            int course = q.remove();
+            res[i++] = course;
+
+            for(int adj : list.get(course)){
+                inDeg[adj]--;
+                if(inDeg[adj] == 0){
+                    q.add(adj);
+                }
             }
         }
-        l.add(i);
-        vis[i] = 1;
-        return false;
+
+        for(int j = 0; j<numCourses; j++){
+            if(inDeg[j] != 0) return new int[0];
+        }
+        return res;
     }
 }
