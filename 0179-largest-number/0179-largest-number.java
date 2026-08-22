c6a012a2d9269087c165440609dabae0010ccbca
jayeshsolanki6 <mejayeshsolanki@gmail.com>
2026-08-22 11:30:30 +0530

    Time: 8 ms (12.28%), Space: 46.2 MB (7.93%) - LeetHub

--- 0179-largest-number/0179-largest-number.java
+++ 0179-largest-number/0179-largest-number.java
@@ -1,31 +1,16 @@
 class Solution {
     public String largestNumber(int[] nums) {
-        PriorityQueue<String> pq = new PriorityQueue<>((a, b) -> {
-            int al = a.length();
-            int bl = b.length();
-            int i = 0, j = 0;
-            while(i < al || j < bl){
-                if(i == al) i = 0;
-                if(j == bl) j = 0;
-                if(a.charAt(i) < b.charAt(j)){
-                    return 1;
-                } else if(a.charAt(i) > b.charAt(j)){
-                    return -1;
-                }
-                i++; j++;
-            }
-            return (al > bl) ? 1 : -1;
-        });
+        PriorityQueue<String> pq = new PriorityQueue<>((a, b) -> (b+a).compareTo(a+b));
 
         for(int i : nums){
-            pq.add("" + i);
+            pq.add(String.valueOf(i));
         }
 
         StringBuilder sb = new StringBuilder();
         while(!pq.isEmpty()){
             sb.append(pq.remove());
         }
-        if(sb.toString().matches("0+")) return "0";
+        if(sb.toString().charAt(0) == '0') return "0";
         return sb.toString();
     }
 }
