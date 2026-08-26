b08463345b7221a58dc45af59ab34bfa4d04218f
jayeshsolanki6 <mejayeshsolanki@gmail.com>
2026-08-26 11:43:29 +0530

    Time: 5 ms (93.73%), Space: 86.8 MB (26.28%) - LeetHub

--- 1406-stone-game-iii/1406-stone-game-iii.java
+++ 1406-stone-game-iii/1406-stone-game-iii.java
@@ -1,29 +1,23 @@
 class Solution {
     public String stoneGameIII(int[] arr) {
         int len = arr.length;
-        int[] dp = new int[len];
-        Arrays.fill(dp, -1);
-        int diff = find(0, len, arr, dp);
+        int[] dp = new int[len+1];
+
+        for(int i = len-1; i>=0; i--){
+            int max = arr[i] - dp[i+1];
+            
+            if(i+2 <= len){
+                max = Math.max(max, arr[i] + arr[i+1] - dp[i+2]);
+            }
+            if(i+3 <= len){
+                max = Math.max(max, arr[i] + arr[i+1] + arr[i+2] - dp[i+3]);
+            }
+
+            dp[i] = max;
+        }
+        int diff = dp[0];
         if(diff == 0) return "Tie";
         if(diff > 0) return "Alice";
         return "Bob";
-    }
-
-    int find(int i, int len, int[] arr, int[] dp){
-        if(i == len) return 0;
-        if(dp[i] != -1) return dp[i];
-        int max = Integer.MIN_VALUE;
-
-        if(len-i >= 1){
-            max = Math.max(max, arr[i] - find(i+1, len, arr, dp));
-        }
-        if(len-i >= 2){
-            max = Math.max(max, arr[i] + arr[i+1] - find(i+2, len, arr, dp));
-        }
-        if(len-i >= 3){
-            max = Math.max(max, arr[i] + arr[i+1] + arr[i+2] - find(i+3, len, arr, dp));
-        }
-
-        return dp[i] = max;
     }
 }
