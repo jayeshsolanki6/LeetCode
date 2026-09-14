ce4d2b633e316359ce4c7257bb5d1a525183314a
jayeshsolanki6 <mejayeshsolanki@gmail.com>
2026-09-14 14:34:16 +0530

    Time: 1 ms (99.81%), Space: 50.2 MB (56.64%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -1,33 +1,20 @@
 class Solution {
-    int[][] memo;
-    int row, col;
     public int minPathSum(int[][] grid) {
-        row = grid.length;
-        col = grid[0].length;
-        memo = new int[row][col];
-        for(int[] i : memo){
-            Arrays.fill(i, -1);
+        int m = grid.length;
+        int n = grid[0].length;
+        int[][] dp = new int[m][n];
+
+        for(int[] row : dp){
+            Arrays.fill(row, -1);
         }
-        return cal(grid, 0, 0);
+        return find(m-1, n-1, grid, dp);
     }
-    int cal(int[][] grid, int m, int n){
-        if(m == row-1){
-            int sum = 0;
-            for(int i = n; i<col; i++){
-                sum += grid[m][i];
-            }
-            return sum;
-        }
-        if(n == col-1){
-            int sum = 0;
-            for(int i = m; i<row; i++){
-                sum += grid[i][n];
-            }
-            return sum;
-        }
-        if(memo[m][n] != -1) return memo[m][n];
-        int ans = grid[m][n] + Math.min(cal(grid, m+1, n), cal(grid, m, n+1));
-        memo[m][n] = ans;
-        return ans;
+    int find(int i, int j, int[][] grid, int[][] dp){
+        if(i == 0 && j == 0) return grid[i][j];
+        if(i < 0 || j < 0) return (int)1e8;
+        if(dp[i][j] != -1) return dp[i][j];
+        int min = Math.min(find(i-1, j, grid, dp), find(i, j-1, grid, dp));
+        return dp[i][j] = grid[i][j] + min;
     }
 }
+
