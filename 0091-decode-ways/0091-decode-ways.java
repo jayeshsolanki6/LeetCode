0923e9ddf824dbbb4376ba478d1de1ad02486468
jayeshsolanki6 <mejayeshsolanki@gmail.com>
2026-09-14 08:25:07 +0530

    Time: 1 ms (81.51%), Space: 42.8 MB (86.9%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.java
+++ 0091-decode-ways/0091-decode-ways.java
@@ -1,9 +1,6 @@
 class Solution {
-    Set<String> set;
     int[] dp;
     public int numDecodings(String s) {
-        set = new HashSet<>();
-        for(int i = 1; i<=26; i++) set.add(""+i);
         dp = new int[s.length()];
         Arrays.fill(dp, -1);
         return find(s.length()-1, s);
@@ -12,11 +9,13 @@ class Solution {
         if(i < 0) return 1;
         if(dp[i] != -1) return dp[i];
         int takeOne = 0, takeTwo = 0;
-        if(i >= 0 && set.contains(s.substring(i, i+1))){
+        if(i >= 0 && s.charAt(i) != '0'){
             takeOne += find(i-1, s);
         }
-        if(i > 0 && set.contains(s.substring(i-1, i+1))){
-            takeTwo += find(i-2, s);
+        if(i > 0 && s.charAt(i-1) != '0'){
+            int num = Integer.parseInt(s.substring(i-1, i+1));
+            if(num <= 26)
+                takeTwo += find(i-2, s);
         }
 
         return dp[i] = takeOne + takeTwo;
