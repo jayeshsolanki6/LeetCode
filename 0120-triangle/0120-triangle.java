89d38f788667fd869a7c83aa2a1e8c9dc704c036
jayeshsolanki6 <mejayeshsolanki@gmail.com>
2026-09-14 14:50:14 +0530

    Time: 1 ms (99.93%), Space: 45.5 MB (50.12%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -1,25 +1,17 @@
 class Solution {
-    public int minimumTotal(List<List<Integer>> triangle) {
-        int n = triangle.size();
-        int[][] dp = new int[n][n];
-        dp[0][0] = triangle.get(0).get(0);
-
-        for(int i = 1; i<n; i++){
-            for(int j = 0; j<=i; j++){
-                if(j == 0){
-                    dp[i][j] = dp[i-1][j] + triangle.get(i).get(j);
-                } else if(j == i){
-                    dp[i][j] = dp[i-1][j-1] + triangle.get(i).get(j);
-                } else{
-                    dp[i][j] = Math.min(dp[i-1][j-1], dp[i-1][j])  + triangle.get(i).get(j);
-                }
-            }
+    public int minimumTotal(List<List<Integer>> tri) {
+        int len = tri.size();
+        int[][] dp = new int[tri.get(len-1).size()][len];
+        for(int[] row : dp){
+            Arrays.fill(row, (int)1e9);
         }
-        int min = Integer.MAX_VALUE;
+        return find(0, 0, len, tri, dp);
+    }
+    int find(int i, int j, int len, List<List<Integer>> tri, int[][] dp){
+        if(i == len) return 0;
+        if(dp[i][j] != (int)1e9) return dp[i][j];
+        int min = Math.min(find(i+1, j, len, tri, dp), find(i+1, j+1, len, tri, dp));
 
-        for(int sum : dp[n-1]){
-            min = Math.min(sum, min);
-        }
-        return min;
+        return dp[i][j] = min + tri.get(i).get(j);
     }
 }
